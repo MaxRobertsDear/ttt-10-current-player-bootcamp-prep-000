c7e28853960600f0212a5edc82180fbf211a58bf
Max <maxhroberts@gmail.com>
2018-10-30 21:33:13 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,7 +3,7 @@ def turn_count ( turn )
   move_count = [] 
   turn.each { |unit| 
     if unit == "X" || unit == "O"
-      move_count << 1 
+      move_count.push(1) 
   }
   move_count.length 
   
